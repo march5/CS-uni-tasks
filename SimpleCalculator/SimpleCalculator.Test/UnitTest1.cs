2ceedeab35fe9169a2850790c4332a2c915920be
Dominik Marchewka <marchewkad09@gmail.com>
2021-11-21 17:17:15 +0100

    UnitTests first commit

--- SimpleCalculator/SimpleCalculator.Test/UnitTest1.cs
+++ SimpleCalculator/SimpleCalculator.Test/UnitTest1.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
 using SimpleCalculator;
+using System.Diagnostics;
+using System;
 
 namespace SimpleCalculator.Test
 {
@@ -11,11 +13,130 @@ namespace SimpleCalculator.Test
         public void TestRegularUseCaseWithSmallNumbers()
         {
             //Arrange:
-            SimpleCalculator.Program.SimpleCalculator simpleCalculator = new SimpleCalculator.Program.SimpleCalculator();
+            Program.SimpleCalculator simpleCalculator = new Program.SimpleCalculator();
             //Act:
             int result = simpleCalculator.Add(1, 2);
             //Assert:
             Assert.AreEqual(3, result);
+        }
+    }
+
+    [TestFixture]
+    public class TextMergerTests
+    {
+        [Test]
+        public void TestRegularValues()
+        {
+            string res = Program.TextMerger.Merge("Ala ma", " kota");
+            string res2 = Program.TextMerger.Merge("Ala ma kota", "");
+            string res3 = Program.TextMerger.Merge("", "Ala ma kota");
+            string res4 = Program.TextMerger.Merge("", "");
+
+            Assert.AreEqual("Ala ma kota", res);
+            Assert.AreEqual("Ala ma kota", res2);
+            Assert.AreEqual("Ala ma kota", res3);
+            Assert.AreEqual("", res4);
+        }
+
+        [Test]
+        public void TestNullStrings()
+        {
+            string res = Program.TextMerger.Merge("Ala ma", null);
+            string res2 = Program.TextMerger.Merge(null, " kota");
+            string res3 = Program.TextMerger.Merge(null, null);
+
+            Assert.AreEqual(null, res);
+            Assert.AreEqual(null, res2);
+            Assert.AreEqual(null, res3);
+        }
+
+        [Test]
+        public void TestNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.TextMerger.MergeNotNull(null, "Ala"));
+            Assert.Throws<ArgumentNullException>(() => Program.TextMerger.MergeNotNull("Ala", null));
+            Assert.Throws<ArgumentNullException>(() => Program.TextMerger.MergeNotNull(null, null));
+        }
+
+        [TestFixture]
+        public class AnagramCheckerTests
+        {
+            [Test]
+            public void TestRegularStrings()
+            {
+                IAnagramChecker anagramChecker = new AnagramChecker();
+
+                bool res = anagramChecker.isAnagram("ala", "ala");
+                Assert.AreEqual(true, res);
+
+                res = anagramChecker.isAnagram("ala  ", "  ala");
+                Assert.AreEqual(true, res);
+
+                res = anagramChecker.isAnagram("mars", "rams");
+                Assert.AreEqual(true, res);
+
+                res = anagramChecker.isAnagram("pear", "reap");
+                Assert.AreEqual(true, res);
+
+                res = anagramChecker.isAnagram("meats", "steam");
+                Assert.AreEqual(true, res);
+
+                res = anagramChecker.isAnagram("pot", "spot");
+                Assert.AreEqual(false, res);
+
+                res = anagramChecker.isAnagram("pins", "spins");
+                Assert.AreEqual(false, res);
+            }
+
+            [Test]
+            public void TestNumbers()
+            {
+                IAnagramChecker anagramChecker = new AnagramChecker();
+
+                bool res = anagramChecker.isAnagram("1234567890", "0987654321");
+                Assert.AreEqual(true, res);
+                res = anagramChecker.isAnagram("2222", "2222");
+                Assert.AreEqual(true, res);
+                res = anagramChecker.isAnagram("1 4 2 3", "1234   ");
+                Assert.AreEqual(true, res);
+            }
+
+            [Test]
+            public void TestAllChar()
+            {
+                IAnagramChecker anagramChecker = new AnagramChecker();
+
+                bool res = anagramChecker.isAnagram("awd923?, ..", "., ?daw392.");
+                Assert.AreEqual(true, res);
+
+                res = anagramChecker.isAnagram("AaaA99213", "aaaa21399");
+                Assert.AreEqual(true, res);
+
+                res = anagramChecker.isAnagram("A    AAAA", "A A A A A");
+                Assert.AreEqual(true, res);
+            }
+
+            [Test]
+            public void TestNullStrings()
+            {
+                IAnagramChecker anagramChecker = new AnagramChecker();
+
+                Assert.Throws<ArgumentNullException>(() => anagramChecker.isAnagram(null, "oko"));
+
+                Assert.Throws<ArgumentNullException>(() => anagramChecker.isAnagram("oko", null));
+
+                Assert.Throws<ArgumentNullException>(() => anagramChecker.isAnagram(null, null));
+            }
+
+            [Test]
+            public void TestEmptyStrings()
+            {
+                IAnagramChecker anagramChecker = new AnagramChecker();
+
+                Assert.Throws<ArgumentException>(() => anagramChecker.isAnagram("", "oko"));
+                Assert.Throws<ArgumentException>(() => anagramChecker.isAnagram("oko", ""));
+                Assert.Throws<ArgumentException>(() => anagramChecker.isAnagram("", ""));
+            }
         }
     }
 }
--- SimpleCalculator/SimpleCalculator/Program.cs
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -1,13 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleCalculator
 {
+    public interface IAnagramChecker
+    {
+        bool isAnagram(string word1, string word2);
+    }
+
+    public class AnagramChecker : IAnagramChecker
+    {
+        public bool isAnagram(string word1, string word2)
+        {
+
+            if (word1 == null || word2 == null) throw new ArgumentNullException();
+            else if (word1 == "" || word2 == "") throw new ArgumentException();
+            else
+            {
+                if (word1.Length != word2.Length) return false;
+
+                var word1Array = word1.ToLower().ToCharArray();
+                var word2Array = word2.ToLower().ToCharArray();
+
+                Array.Sort(word1Array);
+                Array.Sort(word2Array);
+
+                for (int i = 0; i < word1Array.Length; i++)
+                    if (word1Array[i] != word2Array[i]) return false;
+            }
+
+            return true;
+        }
+    }
+
     public class Program
     {
+
+        public class TextMerger
+        {
+            public static string Merge(string a, string b)
+            {
+
+                if (a == null || b == null) return null;
+                return a + b;
+            }
+
+            public static string MergeNotNull(string a, string b)
+            {
+                if (a == null || b == null) throw new ArgumentNullException();
+                else return a + b;
+            }
+        }
+
         public class SimpleCalculator
         {
             public int Add(int a, int b)
