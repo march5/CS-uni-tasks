8bba450fb355bd60c712c9434c62314ddb992d62
Dominik Marchewka <marchewkad09@gmail.com>
2021-12-28 14:01:06 +0100

    MiniProject first commit

---
+++ MiniProject/MiniProject/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
+
+namespace MiniProject
+{
+    class Program
+    {
+        public static int maxCrawlerNum = 100;
+        public static int crawlersCompleted = 0;
+        public static int globalCrawlersCount = 0;
+        public static bool keyWordFound = false;
+        public static List<int> pathLengthsFound = new List<int>();
+
+        public class Crawler
+        {
+            string keyWord;
+            WebClient web;
+            int pathLength;
+            string link;
+            string pageText;
+
+            public Crawler(int pathL, string link, string keyWord)
+            {
+                this.keyWord = keyWord;
+                this.pathLength = pathL + 1;
+                this.link = link;
+                this.web = new WebClient();
+
+                Task t = Task.Run(() => crawl());
+            }
+            
+            string[] forbidden = { "jpg", "Plik:", "Kategoria:", "File:", "Category:", "Wikipedia:", "Simple", "<", ">", "Szablon:", "Pomoc:"
+            , "wikimedia", "https", "Specjalna:", "Dyskusja:", "Help:", "Special:", "special"};
+
+            bool checkLink(string link)
+            {
+                foreach(var s in forbidden)
+                {
+                    if (link.Contains(s)) return false;
+                }
+                return true;
+            }
+
+            public bool ReadPage()
+            {
+                Uri uriResult;
+                bool result = Uri.TryCreate(link, UriKind.Absolute, out uriResult)
+                    && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+                if(result)
+                {
+                    System.IO.Stream stream = web.OpenRead(this.link);
+                    using (System.IO.StreamReader reader = new System.IO.StreamReader(stream))
+                    {
+                        this.pageText = reader.ReadToEnd();
+                    }
+                }
+                return result;
+            }
+
+            public void crawl()
+            {
+                
+                if (ReadPage())
+                {
+                    int index;
+                    int endOfLink;
+                    string linkFound;
+                    string linkBeg = link.Substring(0, 24);
+
+                    Console.WriteLine("Crawling " + link);
+
+                    if (pageText.Contains(keyWord))
+                    {
+                        Console.WriteLine("Hitler key word found through path of length: " + pathLength);
+                        keyWordFound = true;
+                        pathLengthsFound.Add(pathLength);
+                    }
+                    else
+                    {
+                        while(pageText.Length > 0)
+                        {
+                            if(pageText.Contains("<a href="))
+                            {
+                                index = pageText.IndexOf("<a href=\"");
+                                endOfLink = index + 8;
+
+                                while(pageText[endOfLink] != ' ')
+                                    endOfLink++;
+
+                                linkFound = pageText.Substring(index + 9, endOfLink - (index + 10));
+
+                                if(globalCrawlersCount < maxCrawlerNum && this.pathLength < 100 && linkFound.Contains("/wiki/") && checkLink(linkFound))
+                                    {
+                                        new Crawler(pathLength, linkBeg + linkFound, keyWord);
+                                        globalCrawlersCount++;
+                                    }
+                                
+                                pageText = pageText.Substring(endOfLink + 1);
+                            }
+                            else
+                                pageText = "";
+
+                            Thread.Yield();
+                        }
+                    }
+                }
+                crawlersCompleted++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            string randomInitPage = "http://en.wikipedia.org/wiki/Special:Random";
+            string initLink = "https://pl.wikipedia.org/wiki/Toster";
+
+
+            for(int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Starting loop nr: " + i);
+                keyWordFound = false;
+                crawlersCompleted = 0;
+                globalCrawlersCount = 0;
+                new Crawler(0, randomInitPage, "Hitler");
+                while (!keyWordFound && crawlersCompleted < maxCrawlerNum)
+                {
+
+                }
+            }
+
+            Console.WriteLine("Path lengths: ");
+            foreach(var x in pathLengthsFound)
+            {
+                Console.WriteLine(x);
+            }
+            
+        }
+    }
+}
---
+++ Threads/Threads/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace Threads
+{
+    class Program
+    {
+
+        static void PingAddress(string[] input, int start)
+        {   
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+
+            options.DontFragment = true;
+
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+
+            string[] texts;
+
+            PingReply reply;
+            int i = start;
+
+            while(i < start + input.Length/4 + 1 && i < input.Length)
+            {
+                texts = input[i].Split(';');
+                Console.WriteLine(Thread.CurrentThread.Name + " " + i);
+                reply = pingSender.Send(texts[1], timeout, buffer, options);
+                if (reply.Status == IPStatus.Success) Console.WriteLine("Succesfully pinged " + texts[0] + "by thread" + Thread.CurrentThread.Name);
+                i++;
+            }
+        }
+
+        public static long Threads1()
+        {
+            string loc = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"ping.txt");
+
+            string[] text = File.ReadAllLines(loc);
+            Console.WriteLine(text.Length);
+
+            int lengthForOne = text.Length / 4;
+
+            Stopwatch sw1 = new Stopwatch();
+            
+            Thread[] t = new Thread[4];
+            sw1.Start();
+
+            t[0] = new Thread(() => PingAddress(text, 0));
+            t[0].Name = "0";
+            t[1] = new Thread(() => PingAddress(text, lengthForOne));
+            t[1].Name = "1";
+            t[2] = new Thread(() => PingAddress(text, lengthForOne * 2));
+            t[2].Name = "2";
+            t[3] = new Thread(() => PingAddress(text, lengthForOne * 3));
+            t[3].Name = "3";
+
+            t[0].Start();
+            t[1].Start();
+            t[2].Start();
+            t[3].Start();
+
+            t[0].Join();
+            t[1].Join();
+            t[2].Join();
+            t[3].Join();
+
+            sw1.Stop();
+            return sw1.ElapsedMilliseconds;
+        }
+
+        public static async Task PingAddressTask(string input)
+        {
+                Ping pingSender = new Ping();
+                PingOptions options = new PingOptions();
+
+                options.DontFragment = true;
+
+                string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                int timeout = 120;
+
+                string[] texts = input.Split(';');
+
+                Console.WriteLine("Tryign to ping " + texts[0]);
+                pingSender.Send(texts[1], timeout, buffer, options);
+        }
+
+        public static async void TaskPing()
+        {
+            
+            string loc = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"ping.txt");
+
+            string[] text = File.ReadAllLines(loc);
+            Console.WriteLine(text.Length);
+            Stopwatch sw1 = new Stopwatch();
+            sw1.Start();
+
+            await Task.WhenAll(text.Select(x => PingAddressTask(x)));
+
+            sw1.Stop();
+            Console.WriteLine(sw1.ElapsedMilliseconds); 
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Threads1());
+            //TaskPing();
+        }
+    }
+}
