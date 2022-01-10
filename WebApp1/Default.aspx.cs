d920c9d43ce4fc2b302a8363e44b118384034446
Dominik Marchewka <marchewkad09@gmail.com>
2022-01-10 14:06:02 +0100

    WebApp1

---
+++ WebApp1/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private DateTime data;
+
+        public Default()
+        {
+            data = new DateTime();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            setDateToLabel();
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            setDateToLabel();
+        }
+
+        private void setDateToLabel()
+        {
+            data = DateTime.Now;
+            Label1.Text = data.Year + "." +
+                data.Month + "." +
+                data.Day + "." +
+                data.Hour + "." +
+                data.Minute + "." +
+                data.Second + "." +
+                data.Millisecond;
+        }
+    }
+}
